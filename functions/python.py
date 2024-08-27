7d4fcf489825f34bf9eb22732043f49705de13c4
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-08-27 06:39:17 +0300

    python

---
+++ functions/python.py
@@ -0,0 +1,24 @@
+x = 12
+y = 4
+
+total = x + y
+
+print(total)
+
+def function(name):
+	return f'Hello {name}, good morning'
+
+def add_sum(num_1, num_2):
+	return num_1 + num_2
+
+answer = add_sum(40, 4)
+print(answer)
+
+def standardize(result):
+	return (result / 50) * 100
+final = standardize(answer)
+print(final)
+name = 'jane'
+print(f'Hello {name}')
+
+print(function('Godfrey'))
---
+++ loops/loops.py
@@ -0,0 +1,18 @@
+fruits = ["apple", "banana", "cherry", "kiwi", "oranges", "lemons"]
+
+for fruit in fruits:
+    print("-", fruit)
+
+for i in range(5):
+    print("-", i)
+
+# while
+count = 0
+
+while count <= 5:
+    print(count)
+    count += 1
+
+for i in range(3):
+    for j in range(3):
+        print(f"i = {i}, j = {j}")
