c46d92d18c1822fe1c338f8bab3f782d3a45c95b
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-09-09 21:14:38 +0300

    .gitignore

---
+++ functions/test.py
@@ -0,0 +1,39 @@
+a = "Python is cool"
+print(a[7:])
+
+a = "Python is cool"
+print(a[0:6])
+
+a = "Python is cool"
+print(a,a,a); 
+
+print(a[-2])
+
+name = 'Godfrey'
+
+greetings = "Good morning"
+
+print(f"{greetings}, Welcome again, {name}")
+school = "Holberton"
+print(f"{school} school")
+
+print(f"{98} Battery street")
+
+str1 = "Holberton"
+str2 = "School"
+str1 = str1 + " " + str2
+print(f"\nWelcome to {str1}!\n")
+
+str1 = "Holberton"
+str2 = "School"
+str1 = f"{str1} {str2}"
+print(f"Welcome to {str1}!")
+
+print('\n')
+word = "Holberton"
+word_first_3 = word[:3]
+word_last_2 = word[7:]
+middle_word = word[1:8]
+print(word_first_3)
+print(word_last_2)
+print(middle_word)
